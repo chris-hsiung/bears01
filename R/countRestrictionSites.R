3723336e62093a57602882dab3416da2bb96e8bc
chsiung <chsiung@gmail.com>
2020-10-23 09:29:47 -0700

    BstXI namespace export

--- R/countRestrictionSites.R
+++ R/countRestrictionSites.R
@@ -1,16 +1,4 @@
-#' sum total occurrence of restriction sites for a dsDNA sequence (input one strand)
-#'@export
-countBstXI <- function( x ){
-      x <- Biostrings::DNAString(x)
 
-      rc_x <- Biostrings::reverseComplement(x)
-
-      REpattern <- 'CCANNNNNNTGG'
-
-      count <- ( Biostrings::countPattern( Biostrings::DNAString( REpattern), x , fixed = FALSE ) + Biostrings::countPattern( Biostrings::DNAString(REpattern), rc_x , fixed = FALSE ) )/2 #divide by 2 after matching pattern on both strands because this is a palindromic pattern, each pattern will always match once on each strand
-
-      return( count )
-}
 
 countBlpI <- function( x ){
       x <- Biostrings::DNAString(x)
