dcf87520dd483faf8b68aa72a2ce4c41b4305827
Chris Hsiung <chsiung@chriss-air.lan>
2021-11-08 21:51:28 -0800

    add flow functions

---
+++ R/flowjo2df.R
@@ -0,0 +1,51 @@
+#' Function to read csv file exported from flowjo. The original .fcs files from which the .csv file was generated must contain keywords. Parses sampleID from $FIL keyword and platename from $PLATENAME keyword and date from $DATE keyword.
+#' Returns data frame
+#' Intended for use within the function processFlowjoExportDir() for processing an entire directory of .csv files.
+#' @export
+flowjo2df <- function( file ){
+
+      #print(file)
+      lines <- readLines(file)
+      lines2 <- lines[2:length(lines)] ## remove the first line which does not follow patterns
+
+      metainfo <- lines2[ grepl("^\\$|^\\#", lines2) ]
+
+      headercsv <- lines2[ grepl('FSC-A', lines2) & !grepl("^\\$|^\\#|FJ_FCS_VERSION", lines2) ]
+
+      datacsv <- lines2[ !grepl("^\\$|^\\#|FJ_FCS_VERSION", lines2) & !grepl('FSC-A', lines2) ]
+
+      if ( length(datacsv) == 0 ){
+            message( paste0( file, ' contains no data, skipping.'))
+            return( NULL )
+      } else{
+            header <- str_remove_all( unlist( strsplit( headercsv, "," ) ), '\"')
+
+            df <- as.data.frame( matrix( unlist( strsplit( datacsv, ",")), nrow = length(datacsv), byrow=T))
+
+            names(df) <- header
+
+            platename <- str_remove( metainfo[ grepl( '^\\$PLATENAME', metainfo ) ], '\\$PLATENAME,' )
+
+            # assertthat::assert_that( is.character(platename),
+            #                          msg = 'platename must be character')
+
+            filename <- str_remove( metainfo[ grepl( '^\\$FIL', metainfo ) ], '\\$FIL,' )
+
+            filename <- str_remove( filename, '.fcs$' )
+
+            date <- str_remove( metainfo[ grepl('^\\$DATE', metainfo)], '\\$DATE,' )
+
+            # assertthat::assert_that( is.character(filename),
+            #                          msg = 'filename must be character')
+
+            df <- dplyr::mutate( df,
+                                 Sample = filename,
+                                 platename = platename,
+                                 date = date)
+
+            return(df)
+
+      }
+
+
+}
---
+++ R/processFlowjoExportDir.R
@@ -0,0 +1,40 @@
+#' Function to batch process .csv files (containing the substring specified by 'csvstring') exported from flowjo within the directory specified by 'dir'. Directory must contain 'platesetupfile' (a tab-delimited .txt file) that contains a column named 'Sample' that should contain 96well ID's that are matched to the $FIL keyword in the .fcs file exported from the Attune.
+#' Outputs a tab-delimited .txt file with file name specified by 'fileout'.
+#' @export
+processFlowjoExportDir <- function( dir, csvstring = '_P1.csv', fileout = 'P1_combodf.txt', platesetupfile = 'platesetup.txt' ){
+
+      assertthat::assert_that( file.exists( file.path(dir, platesetupfile)), msg = paste0( plastesetupfile, ' does not exist') )
+
+      annodf <- read_delim( file.path( dir, platesetupfile), delim = '\t' )
+
+      csvfiles <- list.files( dir )[ grepl( csvstring, list.files( dir )  ) ]
+
+      assertthat::assert_that( length(csvfiles) > 0, msg = 'no matching csv files in directory' )
+
+      filelist <- vector( mode = 'list', length(csvfiles) )
+
+      for ( f in 1:length(csvfiles) ){
+
+            df <- flowjo2df( file.path( dir, csvfiles[f]) )
+
+            filelist[[f]] <- df
+      }
+
+      dfout <- bind_rows(filelist) %>% #note this binds rows even if there are different columns
+            dplyr::mutate(
+                  csvstring = csvstring
+            )
+
+      # check that all annotations have a matching csv file
+      if ( !(sum( annodf$Sample %in% dfout$Sample ) == length(annodf$Sample) ) ) {
+            message( paste0('these sample annotations do not match any $FIL fields: ', toString(annodf$Sample[!(annodf$Sample %in% dfout$Sample)] ) ) )
+      }
+
+
+      dfout <- dplyr::inner_join( annodf, dfout, by = 'Sample')
+
+
+      write.table( dfout, file.path( dir, fileout ), sep = '\t', col.names = TRUE, row.names = FALSE, quote = FALSE )
+
+      return(dfout)
+}
