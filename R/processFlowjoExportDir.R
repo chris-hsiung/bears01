851740c6e286089ec1ae8fa0ff6aff3550535f4b
Chris Hsiung <chsiung@Chriss-MacBook-Air.local>
2021-11-09 12:50:06 -0800

    processFlowjoExportDir

--- R/processFlowjoExportDir.R
+++ R/processFlowjoExportDir.R
@@ -1,11 +1,13 @@
-#' Function to batch process .csv files (containing the substring specified by 'csvstring') exported from flowjo within the directory specified by 'dir'. Directory must contain 'platesetupfile' (a tab-delimited .txt file) that contains a column named 'Sample' that should contain 96well ID's that are matched to the $FIL keyword in the .fcs file exported from the Attune.
+#' Function to batch process .csv files (containing the substring specified by 'csvstring') exported from flowjo within the directory specified by 'dir'. Directory must contain 'platesetupfile' (a tab-delimited .txt file) that contains column with name specified by 'IDvar' (defaults to 'Sample') that should contain 96well ID's that are matched to the $FIL keyword in the .fcs file exported from the Attune.
 #' Outputs a tab-delimited .txt file with file name specified by 'fileout'.
 #' @export
-processFlowjoExportDir <- function( dir, csvstring = '_P1.csv', fileout = 'P1_combodf.txt', platesetupfile = 'platesetup.txt' ){
+processFlowjoExportDir <- function( dir, csvstring = '_P1.csv', fileout = 'P1_combodf.txt', platesetupfile = 'platesetup.txt', IDvar = 'Sample' ){
 
       assertthat::assert_that( file.exists( file.path(dir, platesetupfile)), msg = paste0( plastesetupfile, ' does not exist') )
 
       annodf <- read_delim( file.path( dir, platesetupfile), delim = '\t' )
+
+      assertthat::assert_that( IDvar %in% names(annodf), msg = paset0( IDvar, 'is not a column name in ', plaetsetupfile ) )
 
       csvfiles <- list.files( dir )[ grepl( csvstring, list.files( dir )  ) ]
 
@@ -31,7 +33,7 @@ processFlowjoExportDir <- function( dir, csvstring = '_P1.csv', fileout = 'P1_co
       }
 
 
-      dfout <- dplyr::inner_join( annodf, dfout, by = 'Sample')
+      dfout <- dplyr::inner_join( annodf, dfout, by = IDvar)
 
 
       write.table( dfout, file.path( dir, fileout ), sep = '\t', col.names = TRUE, row.names = FALSE, quote = FALSE )
