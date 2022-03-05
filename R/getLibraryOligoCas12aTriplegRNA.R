50e874d686eefd898dd3066672bf3a4f1108fbaf
Chris Hsiung <chsiung@chriss-air.lan>
2022-03-04 22:01:14 -0800

    edited documentation

--- R/getLibraryOligoCas12aTriplegRNA.R
+++ R/getLibraryOligoCas12aTriplegRNA.R
@@ -1,11 +1,11 @@
-#' Function to assemble 6-plex oligo for pooled synthesis for Cas12a 6-plex gRNA expression. The oligo will consist of:
+#' Function to assemble 6-plex oligo for pooled synthesis for Cas12a 6-plex gRNA expression. The oligo will consist of: Adaptor5p-BsmbI5p-pos1spacer-DR1- pos2spacer-DR3-pos3spacer-BsmbI3p-Adaptor3p
 #' Assumes the oligo will be PCR'd using user-provided adaptor sequences, then digested with BsmbI and ligated into backbone (e.g. compatible with vectors pRG212, pCH39, pCH49, etc.) The backbone is assumed to contain DRWT 5' to the pos1spacer and DR8 3' to the pos3spacer. The final transcript is: DRWT-pos1spacer-DR1-pos2spacer-DR3-pos3spacer-DR8. DR sequences are taken from Deweirdt et al., Nat. Biotech 2020.
 #' The input spacers and adaptors should not contain BsmbI sites.
 #' The input spacers should not contain >= 4 consecutive T's, and should not start with TTT, because the last base of each DR is a T and this will form an undesirable TTTT at the junction.
 #' Checks final oligo for undesirable BsmbI sites (in excess of the 2 that should be there); if fails, throws error.
 #' Checks for undesirable >= 4 consecutive T's in transcript; if fails, throws error.
 #' Checks final oligo contains valid DNA bases only by calling Biostrings::DNAString(); if fails, throws error.
-#' by Chris Hsiung 2022-03-03
+#' By Chris Hsiung 2022-03-03
 #'@return A list containing oligoname and oligosequence. oligoname is in the format 'pos1name_pos2name_pos3name'.
 #'@export
 
